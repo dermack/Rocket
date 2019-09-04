24ec04289c730cce7f474c894ea5c9a08393a25b
dermack <ronietnt@hotmail.com>
2019-09-03 22:37:21 -0300

    First commit

---
+++ Assets/Rocket.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    Rigidbody rigidbody;
+    AudioSource thrust;
+    [SerializeField] float rcsTrhust = 100f;
+    [SerializeField] float mainTrhust = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        thrust = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK!");
+                break;
+            default:
+                print("dead!");
+                break;
+        }
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up * mainTrhust);
+            if (!thrust.isPlaying)
+            {
+                thrust.Play();
+            }            
+        } else 
+        {
+            thrust.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidbody.freezeRotation = true;
+        
+        float rotationThisFrame = rcsTrhust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidbody.freezeRotation = false;
+    }
+}
